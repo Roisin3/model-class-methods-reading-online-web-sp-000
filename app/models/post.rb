3c1d14c6f1954ec81b4406751d2444b8c4346139
Michael Lynch <michael.lynch8211@gmail.com>
2020-10-13 20:13:59 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,10 +6,10 @@ class Post < ActiveRecord::Base
 
   #put new code here
 
-  def self.by_author(author_id)
-    where(author: author_id)
+  def self.from_today
+    where("created_at >=?", Time.zone.today.beginning_of_day)
   end
-
+   
   def self.old_news
     where("created_at <?", Time.zone.today.beginning_of_day)
   end
